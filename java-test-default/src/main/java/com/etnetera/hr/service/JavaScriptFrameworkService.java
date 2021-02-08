a200ba704d72197a9d4d40e1732927d8dba2baee
I. I. Orlov <57638065+Leviathan-X@users.noreply.github.com>
2021-02-08 12:21:31 +0100

    Update JavaScriptFrameworkService.java

--- java-test-default/src/main/java/com/etnetera/hr/service/JavaScriptFrameworkService.java
+++ java-test-default/src/main/java/com/etnetera/hr/service/JavaScriptFrameworkService.java
@@ -3,7 +3,7 @@ package com.etnetera.hr.service;
 import com.etnetera.hr.data.model.JavaScriptFramework;
 
 public interface JavaScriptFrameworkService {
-    void createFramework(String name, double version, String depreciationDate, int hypeLevel);
+    void addFramework(String name, double version, String depreciationDate, int hypeLevel);
     void updateFramework(Long id, String name, double version, String depreciationDate, int hypeLevel);
     void deleteFramework(Long id);
     JavaScriptFramework getFramework(Long id);
